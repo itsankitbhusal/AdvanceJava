311468aa36341a11e99a0ead0d98197e79ba6dcf
ankitbhusal <ankitbhusal95@gmail.com>
2023-03-20 11:48:00 +0545

    key listner

---
+++ src/KeyDemo.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class KeyDemo extends JFrame implements KeyListener {
+    JLabel label1, label2, label3;
+    JTextField textField1, textField2, textField3;
+    JButton button;
+
+    KeyDemo() {
+        label1 = new JLabel("First Number:");
+        label2 = new JLabel("Second Number:");
+        label3 = new JLabel("Result:");
+
+        textField1 = new JTextField(20);
+        textField2 = new JTextField(20);
+        textField3 = new JTextField(20);
+
+        button = new JButton("Press");
+
+        add(label1);
+        add(textField1);
+        add(label2);
+        add(textField2);
+        add(label3);
+        add(textField3);
+        add(button);
+
+        FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER, 20, 10);
+
+        setVisible(true);
+        setLayout(flowLayout);
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button.addKeyListener(this);
+    }
+
+    public void keyPressed(KeyEvent ke) {
+        int num1 = Integer.parseInt(textField1.getText());
+        int num2 = Integer.parseInt(textField2.getText());
+
+        if (ke.getKeyChar() == 'a' || ke.getKeyChar() == 'A') {
+            int additionResult = num1 + num2;
+            textField3.setText(String.valueOf(additionResult));
+        } else if (ke.getKeyChar() == 's' || ke.getKeyChar() == 'S') {
+            int subResult = num1 - num2;
+            textField3.setText(String.valueOf(subResult));
+        } else {
+            textField3.setText("Press valid key!");
+        }
+
+    }
+
+    public void keyTyped(KeyEvent ke) {
+
+    }
+
+    public void keyReleased(KeyEvent ke) {
+
+    }
+
+
+    public static void main(String[] args) {
+        new KeyDemo();
+    }
+}
